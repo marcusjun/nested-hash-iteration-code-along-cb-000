373dc0fa3dc817f90888fa95534ad5cd7300c053
Marcus Jun <mjun@calstate.edu>
2018-04-08 22:23:11 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -24,8 +24,10 @@ def remove_strawberry(contacts)
   #end
 
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    contact_details_hash.each do |attribute,data|
+      binding.pry
+    end
   end
 
-  
+
 end
