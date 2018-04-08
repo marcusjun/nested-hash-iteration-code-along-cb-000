bcb194e2a3e0c95bc532480cb194533aa1e017b6
Marcus Jun <mjun@calstate.edu>
2018-04-08 22:17:04 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts["Freddy Mercury"])
+def remove_strawberry(contacts)
   contacts.each do |person,data|
     data.each do |attribute,value|
       if attribute == :favorite_ice_cream_flavors
